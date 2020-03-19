eca159e094d268fed5abd064c040db9576863bda
Vladislav Ustinovich <vla4erprod@gmail.com>
2020-03-20 01:36:51 +0300

    start node task

--- script.js
+++ script.js
@@ -58,10 +58,12 @@ let viewFlag = 0; //Определяет вид списка
 
 //Загружаем информацию на сайт
 function render(condition = '') {
-    // str = str.replace(/\s/g, ''); // Удаление пробелов
+    let fragment; //выводимая информация
     informationInner.innerHTML = ""; //Очещаем область с информацией
     let Num = 0; //Количество пользователей на экране
+    let content = '';
     ar = DATA.member; //Получаем массив данных
+    // debugger
     ar.forEach(element => { //перебираем массив пользовталей
         if (element.name.indexOf(condition) != -1 ||
             element.nameNat.indexOf(condition) != -1 ||
@@ -69,80 +71,48 @@ function render(condition = '') {
             element.surnameNat.indexOf(condition) != -1 ||
             condition == '') { //проверка условий поиска
             if (viewFlag === 0)
-                informationInner.insertAdjacentHTML('beforeend',
-                    `<Article class="information__item">
-                <div class="information__bio">
-                    <img src="https://placehold.it/150/333" alt="Photo" class="item__photo" width="150px">
-                    <div>
-                        <h2 class="item__name">${element.name+' '+element.surname}</h2>
-                        <h3 class="item__name_native">${element.nameNat+' '+element.surnameNat}</h3>
-                    </div>
-                </div>
-                <div class="information__addition">
-                    <div class="item__departament">
-                        <i class="fas fa-briefcase"></i>
-                        <span>${element.departament}</span>
-                    </div>
-                    <div class="item__room">
-                        <i class="fas fa-door-closed"></i>
-                        <span>${element.room}</span>
-                    </div>
-                </div>
-            </Article>`);
+                fragment = document.querySelector('#templateInfo').content;
             else
-                informationInner.insertAdjacentHTML('beforeend',
-                    `<Article class="information__item_line">
-            <div class="information__bio_line">
-                <img src="https://placehold.it/150/333" alt="Photo" class="item__photo_line"
-                    width="150px">
-                <div>
-                    <h2 class="item__name_line">${element.name+' '+element.surname}</h2>
-                    <h3 class="item__name_native_line">${element.nameNat+' '+element.surnameNat}</h3>
-                </div>
-            </div>
-            <div class="information__addition_line">
-                <div class="item__departament_line">
-                    <i class="fas fa-briefcase"></i>
-                    <span>${element.departament}</span>
-                </div>
-                <div class="item__room_line">
-                    <i class="fas fa-door-closed"></i>
-                    <span>${element.room}</span>
-                </div>
-            </div>
-        </Article>`
-                );
+                fragment = document.querySelector('#templateInfoLine').content;
+
+            fragment.querySelector(".itemName").textContent = element.name + ' ' + element.surname;
+            fragment.querySelector(".itemNameNative").textContent = element.nameNat + ' ' + element.surnameNat;
+            fragment.querySelector(".itemD").textContent = element.departament;
+            fragment.querySelector(".itemR").textContent = element.room;
+            content += fragment.querySelector('Article').outerHTML;
             Num++;
         }
-        informNum.innerHTML = Num + " employees displayed ";
     });
+    informationInner.insertAdjacentHTML('beforeend', content);
+    informNum.innerHTML = Num;
 }
 
 
+
 //Обработка поиска
-inputFind.onkeyup = (e) => {
+inputFind.addEventListener('keyup', (e) => {
     if (e.key === "Enter")
         render(inputFind.value);
-};
+});
 
-btnFind.onclick = () => render(inputFind.value);
+btnFind.addEventListener('click', () => render(inputFind.value));
 
 
 //Обработка кнопок вида списка
-viewGrid.onclick = () => {
+viewGrid.addEventListener('click', () => {
     viewFlag = 0;
     viewTable.classList.remove('active');
     viewGrid.classList.add('active');
     visibleHeaderLine.style.display = "none";
     render(inputFind.value);
-};
-viewTable.onclick = () => {
+});
+viewTable.addEventListener('click', () => {
     viewFlag = 1;
     viewGrid.classList.remove('active');
     viewTable.classList.add('active');
     visibleHeaderLine.style.display = "flex";
     render(inputFind.value);
-};
+});
 
 
 render();
